9f536071461da5c941c5087bc4bfb710fcc94600
“loribean” <aureliadotlim@gmail.com>
2020-09-09 12:13:19 +0800

    fixed bug

--- models/puppymodel.js
+++ models/puppymodel.js
@@ -149,7 +149,7 @@ let getCheckMatches = (values,callback) => {
   }
 
 let getAllConversationsUsers =(values,callback) =>{
-    let query ="select * from match inner join dog on match.dog_id = dog.id where match.follower_user_id=$1";
+    let query ="select * from match inner join dog on match.dog_id = dog.id where match.follower_user_id=$1 and liked =true";
     dbPoolInstance.query(query,values,(err,result)=>{
         callback(err,result)
     })
